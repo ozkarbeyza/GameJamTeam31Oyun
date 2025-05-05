a05a11d821bb046e4f440b7e6815fad525e07036
ozkarbeyza <147501965+ozkarbeyza@users.noreply.github.com>
2025-05-05 20:26:16 +0300

    Update DoorInteraction.cs

--- scripts/DoorInteraction.cs
+++ scripts/DoorInteraction.cs
@@ -3,13 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class DoorInteraction : MonoBehaviour
 {
-    // Define the scene name for the ending
+    
     public string endingSceneName = "GameCompleteScene";
 
-    // This flag ensures the door can only load the scene after all objects are clicked
+    
     public bool CanLoadEndingScene = false;
 
-    // This method is called when the user clicks on the door (or interacts with the collider)
+    
     private void OnMouseDown()
     {
         if (CanLoadEndingScene)
@@ -18,7 +18,7 @@ public class DoorInteraction : MonoBehaviour
         }
     }
 
-    // This method loads the ending scene
+    
     void LoadEndingScene()
     {
         SceneManager.LoadScene(endingSceneName);
